6e12ef6eb0779fd89f80f83b7ec3efb0ac8ac1ae
Lucas Miranda <lucas-m@outlook.com>
2020-06-07 14:14:37 -0300

    build: changed browser route to hash router

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom'
 import Home from './pages/home'
 import CreatePoint from './pages/CreatePoint'
 
 const Routes = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/register" component={CreatePoint} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
