6406dd0f8eb0fbb46b0828b134037cef11a4a08d
Lucas Miranda <lucas-m@outlook.com>
2020-06-07 14:33:14 -0300

    Updates

---
+++ precache-manifest.0ee42003dbe4c08d48a4221a83ce1734.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa661380454bcf8a20ad9a94100e9f2",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "564b29d8b695a30d5e8c",
+    "url": "/my-app/static/css/main.30fbb85a.chunk.css"
+  },
+  {
+    "revision": "c351d6ef12034beb4fdd",
+    "url": "/my-app/static/js/2.68fdcf84.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/my-app/static/js/2.68fdcf84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "564b29d8b695a30d5e8c",
+    "url": "/my-app/static/js/main.9852e648.chunk.js"
+  },
+  {
+    "revision": "9f638feeba5bac901c3e",
+    "url": "/my-app/static/js/runtime-main.1b04e1f2.js"
+  },
+  {
+    "revision": "fe2283e1f17af7f6d2889b5692937968",
+    "url": "/my-app/static/media/home-background.fe2283e1.svg"
+  },
+  {
+    "revision": "47c4581a1dc536c1ba57fddafd6671b9",
+    "url": "/my-app/static/media/logo.47c4581a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2cccb4dacdb0dc2bbdf8960c3404d94f.js"
+  "/my-app/precache-manifest.0ee42003dbe4c08d48a4221a83ce1734.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
